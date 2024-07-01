b666e7210755d3c3c62cc7dae4d98eebaaa6d2e4
Hemang Sidapara <hemang@logisticinfotech.co.in>
2024-07-01 18:35:40 +0530

    dashboard_view.dart changes

--- lib/Screens/home_screen/dashboard_screen/dashboard_controller.dart
+++ lib/Screens/home_screen/dashboard_screen/dashboard_controller.dart
@@ -1,21 +1,3 @@
-import 'package:cph_stocks/Constants/app_assets.dart';
-import 'package:cph_stocks/Constants/app_strings.dart';
-import 'package:cph_stocks/Routes/app_pages.dart';
 import 'package:get/get.dart';
 
-class DashboardController extends GetxController {
-  List<String> contentRouteList = [
-    Routes.orderDetailsScreen,
-    Routes.challanScreen,
-  ];
-
-  List<String> contentList = [
-    AppStrings.orderDetails,
-    AppStrings.challan,
-  ];
-
-  List<String> contentIconList = [
-    AppAssets.orderDetailsIcon,
-    AppAssets.challanIcon,
-  ];
-}
+class DashboardController extends GetxController {}
--- lib/Screens/home_screen/dashboard_screen/dashboard_view.dart
+++ lib/Screens/home_screen/dashboard_screen/dashboard_view.dart
@@ -101,65 +101,55 @@ class DashboardView extends GetView<DashboardController> {
                   child: SizedBox(height: 2.h),
                 ),
 
-                ///Order Details
-                SliverPadding(
-                  padding: EdgeInsets.only(bottom: 2.h),
-                  sliver: SliverGrid.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 2.h,
-                      childAspectRatio: 1.5,
-                      crossAxisSpacing: 2.h,
-                    ),
-                    itemCount: controller.contentList.length,
-                    itemBuilder: (context, index) {
-                      return ElevatedButton(
-                        onPressed: () {
-                          Get.toNamed(controller.contentRouteList[index]);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          padding: EdgeInsets.zero,
-                          elevation: 4,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: AppColors.LIGHT_SECONDARY_COLOR,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 3.w, vertical: 2.h).copyWith(bottom: 0.5.h, right: 2.w),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Text(
-                                    controller.contentList[index].tr,
-                                    overflow: TextOverflow.fade,
-                                    style: TextStyle(
-                                      color: AppColors.SECONDARY_COLOR,
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 16.sp,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Align(
-                                  alignment: Alignment.bottomRight,
-                                  child: Image.asset(
-                                    controller.contentIconList[index],
-                                    width: 14.w,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
+                ///Order details
+                SliverToBoxAdapter(
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      Get.toNamed(Routes.orderDetailsScreen);
                     },
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.LIGHT_SECONDARY_COLOR,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 2.h),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            width: 65.w,
+                            child: Row(
+                              children: [
+                                Image.asset(
+                                  AppAssets.orderDetailsIcon,
+                                  width: 18.w,
+                                ),
+                                SizedBox(width: 3.w),
+                                Text(
+                                  AppStrings.orderDetails.tr,
+                                  style: TextStyle(
+                                    color: AppColors.SECONDARY_COLOR,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 18.sp,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Image.asset(
+                            AppAssets.frontIcon,
+                            width: 9.w,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ],
